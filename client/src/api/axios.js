0b99dbd81851dbe5c6a4eef0be1276071fc86739
BixbyCodes <tripathiutsav87@gmail.com>
2026-02-28 21:49:42 +0530

    production api url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL || "/api",
   timeout: 10000,
   headers: { "Content-Type": "application/json" },
 });
@@ -15,13 +15,10 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (res) => res,
   (err) => {
-    err.message =
-      err.response?.data?.message ||
-      err.response?.data?.errors?.[0]?.msg ||
-      err.message ||
-      "Something went wrong";
+    err.message = err.response?.data?.message || err.response?.data?.errors?.[0]?.msg || err.message || "Something went wrong";
     return Promise.reject(err);
   }
 );
 
 export default api;
+EOF
